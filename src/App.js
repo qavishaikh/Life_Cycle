eaa876a1f2e3ca0737c0df02e4e8cda3f0d50343
Qavi <qavi.shaikh11@gmail.com>
2023-09-22 11:03:00 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Qavi Shaikh</h1>
-    </div>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <>
+      <div className="App">
+       <h1>Life Cycle Methon In React</h1>
+      </div>
+      </>
+    );
+    }
 }
 
 export default App;
